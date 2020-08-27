f88180fd7574315a1ff084dd228ee0a6e04d568a
Caleb Robinson <calebrob6@gmail.com>
2020-08-27 05:06:34 +0000

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Land Cover Labeling Tool
+# Satellite imagery label tool
+
+This tool provides an easy way to collect a random sample of labels over a given scene of satellite imagery.
+
+Run as: `python server.py --input_fn path/to/imagery.tif --output_fn labels.csv`, where "imagery.tif" is a GeoTIFF (or any other format that GDAL can read) with 3 bands of Byte type data (preferably RGB bands), and "labels.csv" is the file where the output will be written. 
+
+This command will start an HTTP server on a given port (defaults to 4042). Users that visit the host the server is running on will be shown a web page like the one below. On this page they will be sequentially presented a patch of imagery sampled from "imagery.tif" at 3 different zoom levels and asked to label the central pixel. The same patch will be shown multiple times while the server is running so class agreement between users can be calculated.
+
+![Screenshot of the tool](img/screenshot.png)
+
+
+## TODO:
+- Allow user to easily customize the class list (config file that is rendered to index.html by the server)
+- Allow user to pass a list of rasters to be used in the tool instead of just 1
+- Allow user to easily include class descriptions and examples (config file that is rendered to index.html by the server)
--- server.py
+++ server.py
@@ -18,9 +18,6 @@ import rasterio
 
 import uuid
 
-from azure.cosmosdb.table.tableservice import TableService
-from azure.cosmosdb.table.models import Entity
-
 import logging
 
 from multiprocessing import Queue, Process
@@ -35,9 +32,10 @@ global MAX_QUEUE_SIZE, SAMPLE_SIZE
 MAX_QUEUE_SIZE = 256
 SAMPLE_SIZE = 240
 
-global TABLE_SERVICE, OUTPUT_PATH
+global TABLE_SERVICE, INPUT_FN, OUTPUT_FN
 TABLE_SERVICE = None
-OUTPUT_PATH = None
+INPUT_FN = None
+OUTPUT_FN = None
 
 #---------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------
@@ -67,42 +65,21 @@ def record_sample():
     # From https://stackoverflow.com/questions/31405812/how-to-get-client-ip-address-using-python-bottle-framework
     client_ip = bottle.request.environ.get('HTTP_X_FORWARDED_FOR') or bottle.request.environ.get('REMOTE_ADDR')
 
-    user_labels = {
-        "water": 1,
-        "tree" : 2,
-        "field": 3,
-        "built": 4,
-        "unknown": -1
-    }
-    label = user_labels[data["label"]]
-
-    lc_labels = np.array(list(map(int, data["labels"].split(","))))
-    size = int(np.sqrt(lc_labels.shape[0]))
-    midpoint = (size-1)//2
-    lc_labels = lc_labels.reshape(size, size)
-
-    agrees = label == lc_labels[midpoint, midpoint]
-    pct_agrees = np.sum(lc_labels == label) / (size**2)
-
     num_times_labeled = data["num_times_labeled"]
 
-    log_row = [
-        client_ip,
-        str(data["time"]),
-        time.ctime(),
-        data["fn"],
-        str(data["x"]),
-        str(data["y"]),
-        str(data["sample_size"]),
-        str(label),
-        str(num_times_labeled),
-        str(agrees),
-        "%0.4f" % (pct_agrees),
-        data["labels"]
-    ]
-
+    log_row = {
+        "client_ip": client_ip,
+        "out_time": str(data["time"]),
+        "in_time": time.ctime(),
+        "x": data["x"],
+        "y": data["y"],
+        "size": data["sample_size"],
+        "user_label": data["label"],
+        "number_of_times_labeled": num_times_labeled,
+    }
+    
     if num_times_labeled < 4:
-        REPEAT_QUEUE.put((data["fn"], data["x"], data["y"], num_times_labeled+1))
+        REPEAT_QUEUE.put((data["x"], data["y"], num_times_labeled+1))
 
     OUTPUT_QUEUE.put(log_row)
 
@@ -113,23 +90,23 @@ def get_sample():
     bottle.response.content_type = 'application/json'
     data = bottle.request.json
     
-    fn, x, y, sample_size, naip_data, naip_img, lc_img, num_times_labeled = SAMPLE_QUEUE.get()
+    (x, y, imagery, img_patch, num_times_labeled) = SAMPLE_QUEUE.get()
 
     r = 9
-    midpoint = sample_size // 2
+    midpoint = SAMPLE_SIZE // 2
     start_idx = midpoint-r-1
     size = r*2 + 2
     for i in range(start_idx, start_idx+size+1):
         rem = (i//3)%3
         color = [ 0 if rem==2 else 255, 255 if rem==0 else 0, 255 if rem==0 else 0 ]
-        naip_img[start_idx,i] = color
-        naip_img[i,start_idx] = color
-        naip_img[start_idx+size,i] = color
-        naip_img[i,start_idx+size] = color
+        img_patch[start_idx,i] = color
+        img_patch[i,start_idx] = color
+        img_patch[start_idx+size,i] = color
+        img_patch[i,start_idx+size] = color
 
-    img1 = naip_img.copy()
-    img2 = naip_img[60:-60,60:-60].copy()
-    img3 = naip_img[start_idx+1:start_idx+size,start_idx+1:start_idx+size].copy()
+    img1 = img_patch.copy()
+    img2 = img_patch[60:-60,60:-60].copy()
+    img3 = img_patch[start_idx+1:start_idx+size,start_idx+1:start_idx+size].copy()
 
     img1 = cv2.imencode(".jpg", cv2.cvtColor(img1, cv2.COLOR_RGB2BGR))[1].tostring()
     img1 = base64.b64encode(img1).decode("utf-8")
@@ -151,7 +128,7 @@ def get_sample():
 
     #print("Image 3 size: %d" % (len(img3)))
 
-    img4 = naip_data.copy()
+    img4 = imagery.copy()
     cv2.circle(img4, (x, y), 100, (255,0,0), thickness=-1)
     img4 = cv2.resize(img4, (0,0), fx=0.1, fy=0.1, interpolation=cv2.INTER_AREA) 
     img4 = cv2.imencode(".jpg", cv2.cvtColor(img4, cv2.COLOR_RGB2BGR))[1].tostring()
@@ -161,11 +138,9 @@ def get_sample():
     #print("Image 4 size: %d" % (len(img4)))
 
     data["time"] = time.ctime()
-    data["fn"] = fn
     data["x"] = x
     data["y"] = y
-    data["sample_size"] = sample_size
-    data["labels"] = ",".join(lc_img[midpoint-r:midpoint+r+1, midpoint-r:midpoint+r+1].flatten().astype(str))
+    data["sample_size"] = SAMPLE_SIZE
     data["num_times_labeled"] = num_times_labeled
 
     bottle.response.status = 200
@@ -177,65 +152,32 @@ def get_sample():
 
 def data_loader_process():
 
-    f = open("data/pa_fns.csv", "r")
-    naip_fns = []
-    lc_fns = []
-    lines = f.read().strip().split("\n")
-    for line in lines:
-        t_fns = line.split(",")
-        naip_fns.append(t_fns[0])
-        lc_fns.append(t_fns[1])
-    f.close()
-
-    naip_tiles = []
-    lc_tiles = []
-    for i in range(len(naip_fns)):
-        print("Loading %d/%d" % (i+1, len(naip_fns)))
-
-        f = rasterio.open(naip_fns[i], "r")
-        naip_data = f.read()
-        naip_data = np.rollaxis(naip_data, 0, 3)
-        f.close()
-
-        f = rasterio.open(lc_fns[i], "r")
-        lc_data = f.read()
-        lc_data = lc_data.squeeze()
-        lc_data[lc_data == 5] = 4
-        lc_data[lc_data == 6] = 4
-        lc_data[lc_data > 6] = 0
-        f.close()
-
-        naip_tiles.append(naip_data)
-        lc_tiles.append(lc_data)
+    nodata_val = 0.0
+    with rasterio.open(INPUT_FN, "r") as f:
+        imagery = np.rollaxis(f.read(), 0, 3)
+        nodata_val = f.nodata
+        assert imagery.shape[2] == 3, "3-band RGB imagery required"
     print("Finished pre-loading data")
 
     i = 0
     while True:
         if SAMPLE_QUEUE.qsize() < MAX_QUEUE_SIZE:
-            
-            num_times_labeled = 0
-
             print("[%s]\tSAMPLE_QUEUE: %d\tREPEAT_QUEUE: %d\tOUTPUT_QUEUE: %d" % (time.ctime(), SAMPLE_QUEUE.qsize(), REPEAT_QUEUE.qsize(), OUTPUT_QUEUE.qsize()))
-
+            
             if not REPEAT_QUEUE.empty():
-                fn, x, y, num_times_labeled = REPEAT_QUEUE.get()
-                idx = naip_fns.index(fn)
-                
-                naip_data = naip_tiles[idx]
-                lc_data = lc_tiles[idx]
+                x, y, num_times_labeled = REPEAT_QUEUE.get()
+                img_patch = imagery[y:y+SAMPLE_SIZE, x:x+SAMPLE_SIZE]
             else:
-                idx = np.random.randint(0, len(naip_fns))
-                fn = naip_fns[idx]
-                naip_data = naip_tiles[idx]
-                lc_data = lc_tiles[idx]
+                num_times_labeled = 0
+                
+                any_nodata = True
+                while any_nodata:
+                    x = np.random.randint(0, imagery.shape[1]-SAMPLE_SIZE)
+                    y = np.random.randint(0, imagery.shape[0]-SAMPLE_SIZE)
+                    img_patch = imagery[y:y+SAMPLE_SIZE, x:x+SAMPLE_SIZE]
+                    any_nodata = np.any(np.sum(img_patch == nodata_val, axis=2) == 3)
 
-                x = np.random.randint(0, naip_data.shape[1]-SAMPLE_SIZE)
-                y = np.random.randint(0, naip_data.shape[0]-SAMPLE_SIZE)
-
-            naip_img = naip_data[y:y+SAMPLE_SIZE, x:x+SAMPLE_SIZE, :3]
-            lc_img = lc_data[y:y+SAMPLE_SIZE, x:x+SAMPLE_SIZE]
-
-            SAMPLE_QUEUE.put((fn, x, y, SAMPLE_SIZE, naip_data[:,:,:3], naip_img, lc_img, num_times_labeled))
+            SAMPLE_QUEUE.put((x, y, imagery, img_patch, num_times_labeled))
             
             i += 1
         else:
@@ -246,35 +188,24 @@ def data_loader_process():
 #---------------------------------------------------------------------------------------
 
 def print_process():
-    
-    using_azure_tables = TABLE_SERVICE is not None
-
     while True:
         if not OUTPUT_QUEUE.empty():
             row = OUTPUT_QUEUE.get()
 
-            if using_azure_tables:
-                log_row = {
-                    "PartitionKey": str(np.random.randint(0, 16)),
-                    "RowKey": str(uuid.uuid4()),
-                    "client_ip": row[0],
-                    "out_time": row[1],
-                    "in_time": row[2],
-                    "file_name": row[3],
-                    "x": row[4],
-                    "y": row[5],
-                    "size": row[6],
-                    "user_label": row[7],
-                    "number_of_times_labeled": row[8],
-                    "agrees_with_ground_truth": row[9],
-                    "percent_agreement_of_19m_neighborhood": row[10],
-                    "neighborhood_19m": row[11]
-                }
-                TABLE_SERVICE.insert_entity("randomlabeltool", log_row)
-            else:
-                f = open(OUTPUT_PATH, "a")
-                f.write("%s\n" % (",".join(row)))
-                f.close()
+            keys = sorted(row.keys())
+
+            if not os.path.exists(OUTPUT_FN):
+                with open(OUTPUT_FN, "w") as f:
+                    f.write("%s\n" % (
+                        ",".join(keys)
+                    ))
+                   
+            with open(OUTPUT_FN, "a") as f:
+                for i, key in enumerate(keys):
+                    f.write(str(row[key]))
+                    if i < len(keys)-1:
+                        f.write(",")
+                f.write("\n")
         else:
             time.sleep(0.5)
 #---------------------------------------------------------------------------------------
@@ -293,34 +224,24 @@ def everything_else(filepath):
 #---------------------------------------------------------------------------------------
 
 def main():
-    global TABLE_SERVICE, OUTPUT_PATH
+    global TABLE_SERVICE, INPUT_FN, OUTPUT_FN
     parser = argparse.ArgumentParser(description="Server")
 
     parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose debugging", default=False)
     parser.add_argument("--host", action="store", dest="host", type=str, help="Host to bind to", default="0.0.0.0")
     parser.add_argument("--port", action="store", dest="port", type=int, help="Port to listen on", default=4042)
     
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--azure_table', action="store_true", dest="azure_table")
-    group.add_argument("--output_path", action="store", dest="output_path", type=str, help="Path to directory where output will be stored")
-
+    parser.add_argument("--input_fn", action="store", dest="input_fn", type=str, help="Path to filename to crop samples from")
+    parser.add_argument("--output_fn", action="store", dest="output_fn", type=str, help="Path to directory where output will be stored")
     parser.add_argument("--seed_data_fn", action="store", dest="seed_data_fn", type=str, help="Filename of seed data to put in the queue", default=None)
 
     args = parser.parse_args(sys.argv[1:])
 
-    if args.azure_table:
-        assert "AZURE_ACCOUNT_NAME" in os.environ
-        assert "AZURE_ACCOUNT_KEY" in os.environ
-        print("Setting up TABLE_SERVICE")
-        TABLE_SERVICE = TableService(
-            account_name=os.environ['AZURE_ACCOUNT_NAME'],
-            account_key=os.environ['AZURE_ACCOUNT_KEY']
-        )
-    else:
-        assert not os.path.exists(args.output_path), "The output file already exists, data would be overwritten"
-        print("Setting up OUTPUT_PATH")
-        OUTPUT_PATH = args.output_path
 
+    assert not os.path.exists(args.output_fn), "The output file already exists, data would be overwritten, exiting..."
+    OUTPUT_FN = args.output_fn
+    assert os.path.exists(args.input_fn), "The input file does not exist, exiting..."
+    INPUT_FN = args.input_fn
     
     if args.seed_data_fn is not None:
 
